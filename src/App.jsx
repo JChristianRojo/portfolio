ebd3555b5ec7489577d1e7de16b31f36838ba02c
JChristianRojo <johnrojo88@yahoo.com>
2024-01-29 04:51:45 +0800

    fix: import component name capitalization in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About/About"
-import Contact from "./components/Contact/contact"
-import Hero from "./components/Hero/hero"
+import Contact from "./components/Contact/Contact"
+import Hero from "./components/Hero/Hero"
 import Navbar from "./components/NavBar/Navbar"
 import Project from "./components/Project/Project"
 
