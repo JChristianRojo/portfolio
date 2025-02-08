78dcaa6b21f91beb8a13b0516bb75937372d6544
JChristianRojo <jchristianrojo@gmail.com>
2025-02-08 20:06:42 +0800

    fix error

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,8 @@
-import About from "./components/About/About"
-import Contact from "./components/Contact/Contact"
-import Hero from "./components/Hero/Hero"
-import Navbar from "./components/NavBar/Navbar"
-import Project from "./components/Project/Project"
+import About from "../components/About/About"
+import Contact from "../components/Contact/Contact"
+import Hero from "../components/Hero/Hero"
+import Navbar from "../components/NavBar/Navbar"
+import Project from "../components/Project/Project"
 
 function App() {
 
